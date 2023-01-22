e0dd781714a7f8c85099ff73b6f0b9fc474775a7
vsh-dev <v.shumeliak.edu@gmail.com>
2023-01-22 20:33:10 +0200

    refactor: rename classes

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Interest() {
   return (
     <>
-      <h3 className="interest--title h3--header">Interests</h3>
-      <p className="interest--text text">
+      <h3 className="interests--title h3--header">Interests</h3>
+      <p className="interests--text text">
         Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
         Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
       </p>
