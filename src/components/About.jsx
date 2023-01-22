8e30595887162a06e44c664a0a9f9acf9870788b
vsh-dev <v.shumeliak.edu@gmail.com>
2023-01-22 20:24:52 +0200

    feat: About and Interest component markup

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <>
+      <h3 className="about--title h3--header">About</h3>
+      <p className="about--text text">
+        I am a frontend developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
+      </p>
+    </>
+  );
+}
---
+++ src/components/Interests.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Interest() {
+  return (
+    <>
+      <h3 className="interest--title h3--header">Interests</h3>
+      <p className="interest--text text">
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </>
+  );
+}
